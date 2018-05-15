ff372917b0a86378f384811d6a0946d95dd06df8
Bartl0miej <bartlomiej.badora@britenet.com.pl>
2018-05-15 12:02:28 +0200

    Challenge 2 passed

---
+++ src/aura/BoatSearchForm/BoatSearchFormController.js
@@ -0,0 +1,40 @@
+({
+    doInit : function(component, event, helper) {
+        let isEnabled = $A.get("e.force:createRecord");
+
+        if (isEnabled) {
+            component.set("v.createRecordEnabled", true);
+        }
+
+        let action = component.get("c.getBoatTypes");
+        action.setCallback(this, function(response) {
+            let list = response.getReturnValue();
+            component.set("v.boatTypes", list);
+        });
+
+        $A.enqueueAction(action);
+    },
+
+    createRecord : function(component, event, helper) {
+        let boatType = component.get("v.boatType");
+        let boatTypesList = component.get("v.boatTypes");
+        for (let i = 0; i < boatTypesList.length; i++) {
+            if (boatType === boatTypesList[i].Name) {
+                boatType = boatTypesList[i].Id;
+                break;
+            }
+        }
+        console.log(boatType);
+        if (boatType === "") {
+            boatType = null;
+        }
+        let createRecordEvent = $A.get("e.force:createRecord");
+        createRecordEvent.setParams( {
+           "entityApiName" : "Boat__c",
+           "defaultFieldValues" : {
+               'BoatType__c' : boatType
+           }
+        });
+        createRecordEvent.fire();
+    }
+})
---
+++ src/aura/FriendswithBoats/FriendswithBoats.app
@@ -0,0 +1,9 @@
+<aura:application description="FriendswithBoats" extends="force:slds">
+    <lightning:layout horizontalAlign="center">
+        <lightning:layoutItem>
+            <lightning:card title="Find a Boat">
+                <c:BoatSearchForm/>
+            </lightning:card>
+        </lightning:layoutItem>
+    </lightning:layout>
+</aura:application>
