7401af8dfd68f13ad0ad687200c55b4f74462dfd
Bartl0miej <bartlomiej.badora@britenet.com.pl>
2018-05-15 16:13:13 +0200

    After challenge 4

--- src/aura/BoatSearch/BoatSearchController.js
+++ src/aura/BoatSearch/BoatSearchController.js
@@ -1,6 +1,7 @@
 ({
     onFormSubmit : function(component, event, helper) {
         let typeId = event.getParam("formData");
+        //let boatTypeId = typeId.boatTypeId;
         console.log('in boatSearch:' + typeId);
         let childComponent = component.find("searchResultComponent");
         childComponent.search(typeId);
--- src/aura/BoatSearchResults/BoatSearchResultsController.js
+++ src/aura/BoatSearchResults/BoatSearchResultsController.js
@@ -3,18 +3,25 @@
         helper.onSearch(component);
     },
 
-    search : function(component, event, helper) {
+/*    search : function(component, event, helper) {
         let args = event.getParam('arguments');
         let typeId = args.boatTypeId;
         component.set("v.boatTypeId", typeId);
         helper.onSearch(component);
-        /*console.log('tajp ajdi: ' + typeId);*/
+        *//*console.log('tajp ajdi: ' + typeId);*//*
         component.get("c.doSearch");
 
-    },
+    },*/
 
     doSearch : function(component, event, helper) {
-        component.get("v.boatTypeId");
+        console.log('in do search method');
+        let args = event.getParam('arguments');
+        let typeId = args.boatTypeId;
+        component.set("v.boatTypeId", typeId);
+        console.log('tajp ajdi: ' + typeId);
+
+
+        //component.get("v.boatTypeId");
         helper.onSearch(component);
     }
 })
